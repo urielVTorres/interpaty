b0251784772b3261a4888b1d9e98ae57c2e8492d
Marco Uriel Vargas Torres <m.urielv.torres@gmail.com>
2022-11-08 12:50:25 -0600

    Se agregó redireccionamiento si no está loggeado

--- src/layout/RutaProtegida.js
+++ src/layout/RutaProtegida.js
@@ -1,9 +1,26 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
 import Header from '../components/Header.js'
 import Footer from '../components/Footer.js'
+import axios from 'axios'
 const RutaProtegida = () => {
   const id = localStorage.getItem('key');
+  const navigate = useNavigate();
+  
+  useEffect(()=>{
+    const isLogged = async ()=>{
+        const {data} = await axios(`${process.env.REACT_APP_URL_BACKEND}/reporte`, {
+          headers: {
+            'Content-Type': 'application/json;charset=UTF-8',
+            'Access-Control-Allow-Origin': '*'
+          }
+        });
+        if(data.id !== id)
+          navigate('/login');
+    }
+    isLogged();
+  }, []);
+
   return (
     <>
       {id? 
